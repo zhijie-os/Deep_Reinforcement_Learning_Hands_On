d6c0914fa72afbc9adf82dd8cf1cc6afa9a1c8ec
Zhijie.Xia <zhijiexiacs@gmail.com>
2025-12-25 17:22:36 +0800

    [Chapter8]dqn distr

--- Chapter8/lib/dqn_extra.py
+++ Chapter8/lib/dqn_extra.py
@@ -9,6 +9,94 @@ import typing as tt
 BETA_START = 0.4
 BETA_FRAMES = 100000
 
+# distributional DQN params
+Vmax = 10
+Vmin = -10
+N_ATOMS = 51
+DELTA_Z = (Vmax - Vmin) / (N_ATOMS- 1)
+
+class DistributionalDQN(nn.Module):
+    def __init__(self, input_shape: tt.Tuple[int, ...], n_actions: int):
+        super(DistributionalDQN, self).__init__()
+
+        self.conv = nn.Sequential(
+            nn.Conv2d(input_shape[0], 32, kernel_size=8, stride=4),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1),
+            nn.ReLU(),
+            nn.Flatten()
+        )
+        size = self.conv(torch.zeros(1, *input_shape)).size()[-1]
+        self.fc = nn.Sequential(
+            nn.Linear(size, 512),
+            nn.ReLU(),
+            nn.Linear(512, n_actions * N_ATOMS)
+        )
+
+        sups = torch.arange(Vmin, Vmax + DELTA_Z, DELTA_Z)
+        self.register_buffer("supports", sups)
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, x: torch.ByteTensor) -> torch.Tensor:
+        batch_size = x.size()[0]
+        xx = x / 255
+        fc_out = self.fc(self.conv(xx))
+        return fc_out.view(batch_size, -1, N_ATOMS)
+
+    def both(self, x: torch.ByteTensor) -> tt.Tuple[torch.Tensor, torch.Tensor]:
+        cat_out = self(x)
+        probs = self.apply_softmax(cat_out)
+        weights = probs * self.supports
+        res = weights.sum(dim=2)
+        return cat_out, res
+
+    def qvals(self, x: torch.ByteTensor) -> torch.Tensor:
+        return self.both(x)[1]
+
+    def apply_softmax(self, t: torch.Tensor) -> torch.Tensor:
+        return self.softmax(t.view(-1, N_ATOMS)).view(t.size())
+
+def distr_projection(next_distr: np.ndarray, rewards: np.ndarray,
+                     dones: np.ndarray, gamma: float):
+    """
+    Perform distribution projection aka Catergorical Algorithm from the
+    "A Distributional Perspective on RL" paper
+    """
+    batch_size = len(rewards)
+    proj_distr = np.zeros((batch_size, N_ATOMS), dtype=np.float32)
+    delta_z = (Vmax - Vmin) / (N_ATOMS - 1)
+    for atom in range(N_ATOMS):
+        v = rewards + (Vmin + atom * delta_z) * gamma
+        tz_j = np.minimum(Vmax, np.maximum(Vmin, v))
+        b_j = (tz_j - Vmin) / delta_z
+        l = np.floor(b_j).astype(np.int64)
+        u = np.ceil(b_j).astype(np.int64)
+        eq_mask = u == l
+        proj_distr[eq_mask, l[eq_mask]] += next_distr[eq_mask, atom]
+        ne_mask = u != l
+        proj_distr[ne_mask, l[ne_mask]] += next_distr[ne_mask, atom] * (u - b_j)[ne_mask]
+        proj_distr[ne_mask, u[ne_mask]] += next_distr[ne_mask, atom] * (b_j - l)[ne_mask]
+    if dones.any():
+        proj_distr[dones] = 0.0
+        tz_j = np.minimum(Vmax, np.maximum(Vmin, rewards[dones]))
+        b_j = (tz_j - Vmin) / delta_z
+        l = np.floor(b_j).astype(np.int64)
+        u = np.ceil(b_j).astype(np.int64)
+        eq_mask = u == l
+        eq_dones = dones.copy()
+        eq_dones[dones] = eq_mask
+        if eq_dones.any():
+            proj_distr[eq_dones, l[eq_mask]] = 1.0
+        ne_mask = u != l
+        ne_dones = dones.copy()
+        ne_dones[dones] = ne_mask
+        if ne_dones.any():
+            proj_distr[ne_dones, l[ne_mask]] = (u - b_j)[ne_mask]
+            proj_distr[ne_dones, u[ne_mask]] = (b_j - l)[ne_mask]
+    return proj_distr
+
 class DuelingDQN(nn.Module):
     def __init__(self,input_shape, n_actions):
         super(DuelingDQN, self).__init__()

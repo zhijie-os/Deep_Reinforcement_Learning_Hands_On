0b4386e8440fc8ee3ca2312b3b884ed0f334bbf7
Zhijie Xia <zhijiexiacs@gmail.com>
2026-02-09 00:25:09 +0800

    [Chapter15] skip D4PG for now

--- Chapter15/lib/model.py
+++ Chapter15/lib/model.py
@@ -128,3 +128,52 @@ class AgentDDPG(ptan.agent.BaseAgent):
 
         actions = np.clip(actions, -1, 1)
         return actions, new_a_states
+
+
+class D4PGCritic(nn.Module):
+    def __init__(self, obs_size: int, act_size: int,
+                 n_atoms: int, v_min: float, v_max: float):
+        super(D4PGCritic, self).__init__()
+
+        self.obs_net = nn.Sequential(
+            nn.Linear(obs_size, 400),
+            nn.ReLU(),
+        )
+
+        self.out_net = nn.Sequential(
+            nn.Linear(400 + act_size, 300),
+            nn.ReLU(),
+            nn.Linear(300, n_atoms)
+        )
+
+        delta = (v_max - v_min) / (n_atoms - 1)
+        self.register_buffer("supports", torch.arange(v_min, v_max + delta, delta))
+
+    def forward(self, x: torch.Tensor, a: torch.Tensor):
+        obs = self.obs_net(x)
+        return self.out_net(torch.cat([obs, a], dim=1))
+
+    def distr_to_q(self, distr: torch.Tensor):
+        weights = F.softmax(distr, dim=1) * self.supports
+        res = weights.sum(dim=1)
+        return res.unsqueeze(dim=-1)
+
+
+class AgentD4PG(ptan.agent.BaseAgent):
+    """
+    Agent implementing noisy agent
+    """
+    def __init__(self, net: DDPGActor, device: torch.device = torch.device("cpu"),
+                 epsilon: float = 0.3):
+        self.net = net
+        self.device = device
+        self.epsilon = epsilon
+
+    def __call__(self, states: ptan.agent.States, agent_states: ptan.agent.AgentStates):
+        states_v = ptan.agent.float32_preprocessor(states)
+        states_v = states_v.to(self.device)
+        mu_v = self.net(states_v)
+        actions = mu_v.data.cpu().numpy()
+        actions += self.epsilon * np.random.normal(size=actions.shape)
+        actions = np.clip(actions, -1, 1)
+        return actions, agent_states
